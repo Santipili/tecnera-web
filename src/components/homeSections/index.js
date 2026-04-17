2c9c3eb776b6a74318e7e21a9a39cc05ef31a271
Santipili <santi_mdq@live.com.ar>
2026-04-17 11:48:41 -0300

    public and src move to app

--- src/components/homeSections/index.js
+++
@@ -1,6 +0,0 @@
-import FAQ from "./FAQ";
-import Features from "./Features";
-import HeroCarousel from "./HeroCarousel";
-import Products from "./Products";
-
-export { HeroCarousel, Features, Products, FAQ };
